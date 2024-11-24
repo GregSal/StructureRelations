c0d9182b8a444f6fecd0c5c16de9df7d735d0545
GregSal <33795111+GregSal@users.noreply.github.com>
2024-11-23 19:09:19 -0500

    rounding handles coordinates with z axis

--- src/utilities.py
+++ src/utilities.py
@@ -20,11 +20,20 @@ def point_round(point: shapely.Point, precision: int = PRECISION)->List[float]:
             Defaults to global PRECISION value.
 
     Returns:
-        List[float]: A list of rounded point coordinates.
+        shapely.Point: A shapely point with coordinates rounded to the
+            specified precision.
     '''
-    x, y = shapely.get_coordinates(point)[0]
+    dim = shapely.get_coordinate_dimension(point)
+    if dim == 2:
+        x, y = shapely.get_coordinates(point, include_z=True)[0]
+        clean_coords = [round(x,precision), round(y,precision)]
+    elif dim == 3:
+        x, y, z = shapely.get_coordinates(point, include_z=True)[0]
+        clean_coords = [round(x,precision), round(y,precision), round(z,precision)]
+    else:
+        raise ValueError(f"Invalid coordinate dimension: {dim}")
     clean_coords = (round(x,precision), round(y,precision))
-    return clean_coords
+    return shapely.Point(clean_coords)
 
 
 def poly_round(polygon: shapely.Polygon, precision: int = PRECISION)->shapely.Polygon:
@@ -40,7 +49,14 @@ def poly_round(polygon: shapely.Polygon, precision: int = PRECISION)->shapely.Po
         shapely.Polygon: The supplied polygon with all coordinate points
             rounded to the supplied precision.
     '''
-    polygon_points = [(round(x,precision), round(y,precision))
-                      for x,y in shapely.get_coordinates(polygon)]
+    dim = shapely.get_coordinate_dimension(polygon)
+    if dim == 2:
+        polygon_points = [(round(x,precision), round(y,precision))
+                          for x,y in shapely.get_coordinates(polygon, include_z=True)]
+    elif dim == 3:
+        polygon_points = [(round(x,precision), round(y,precision), round(z,precision))
+                          for x,y,z in shapely.get_coordinates(polygon, include_z=True)]
+    else:
+        raise ValueError(f"Invalid coordinate dimension: {dim}")
     clean_poly = shapely.Polygon(polygon_points)
     return clean_poly
