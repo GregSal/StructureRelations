d825be982a11ef43f32508a3594de277b8046373
GregSal <33795111+GregSal@users.noreply.github.com>
2025-05-04 20:33:31 -0400

    Completed TestBoundaryContourGeneration

--- Tests/test_contour_graph.py
+++ Tests/test_contour_graph.py
@@ -20,6 +20,9 @@ def make_test_contour_table():
     The table is sorted by ROI, Slice and by descending area.
 
     The test table contains the following data:
+        ROI 0:
+            slices, 0.0, 1.0, 2.0, 3.0
+            1 contour per slice with area 1.0.
         ROI 1:
             slices, 1.0, 2.0,
             1 contour per slice with area 9.0.
@@ -30,12 +33,17 @@ def make_test_contour_table():
                 two with area 1.0, and
                 two with area 4.0.
     '''
+    box1= box_points(width=1)
     box1_left = box_points(width=1, offset_x=1.5)
     box1_right = box_points(width=1, offset_x=-1.5)
     box2_left = box_points(width=2, offset_x=1.5)
     box2_right = box_points(width=2, offset_x=-1.5)
     box3_right = box_points(width=3, offset_x=-1.5)
     slice_data = [
+        ContourPoints(box1, roi=0, slice_index=0.0),  # ROI 0, Area 1
+        ContourPoints(box1, roi=0, slice_index=1.0),  # ROI 0, Area 1
+        ContourPoints(box1, roi=0, slice_index=2.0),  # ROI 0, Area 1
+        ContourPoints(box1, roi=0, slice_index=3.0),  # ROI 0, Area 1
         ContourPoints(box1_left, roi=2, slice_index=0.0),  # ROI 2, Area 1
         ContourPoints(box2_left, roi=2, slice_index=0.0),  # ROI 2, Area 4
         ContourPoints(box1_left, roi=2, slice_index=1.0),  # ROI 2, Area 1
@@ -94,6 +102,8 @@ class TestBuildContours():
 class TestAddGraphEdges():
     '''Test the add_graph_edges function.'''
     def test_add_graph_edges(self):
+        '''Test the add_graph_edges function generates the correct number of
+        edges.'''
         contour_table, slice_sequence = make_test_contour_table()
         contour_graph = make_bare_networks_graph_for_testing(contour_table,
                                                              roi=1)
@@ -107,6 +117,181 @@ class TestAddGraphEdges():
         # Check that the graph for ROI 2 has two edges between the two slices.
         assert contour_graph.number_of_edges() == 2
 
+    def test_edge_match(self):
+        '''Test the match data is correct for an edge.'''
+        contour_table, slice_sequence = make_test_contour_table()
+        contour_graph = make_bare_networks_graph_for_testing(contour_table,
+                                                             roi=1)
+        contour_graph = add_graph_edges(contour_graph, slice_sequence)
+        # Check that the polygons in the match are correct.
+        contour1_selection = ((contour_table['ROI'] == 1) &
+                              (contour_table['Slice'] == 1.0))
+        contour2_selection = ((contour_table['ROI'] == 1) &
+                              (contour_table['Slice'] == 1.0))
+        polygon1 = contour_table[contour1_selection].Polygon.tolist()[0]
+        polygon2 = contour_table[contour2_selection].Polygon.tolist()[0]
+        contour_match = list(contour_graph.edges.data('match'))[0][2]
+        assert polygon1 == contour_match.contour1.polygon
+        assert polygon2 == contour_match.contour2.polygon
+        assert contour_match.gap == 1.0
+
+class TestBoundaryContourGeneration():
+    '''Test the add_boundary_contours function.
+        Test the boundary contour:
+            - The slice index of the boundary contour is half way between the
+                boundary contour slice and the neighbour slice that is not
+                linked with an edge.
+            - IsInterpolated is True
+            - IsBoundary is True
+            - IsHole matches the IsHole value of the non-interpolated Contour
+            - The area of the interpolated contour is less than the area of the
+                non-interpolated Contour
+        Test the edge between the original contour and the interpolated contour:
+            - The ContourMatch contains references to the original contour
+                and the interpolated contour.
+            - The gap is the distance between the original contour and the
+                interpolated contour.
+        Test the changes to the SliceSequence:
+            - The SliceSequence contains the interpolated contour slice index.
+            - The interpolated contour slice index has 'Original' = False.
+            - The 'PreviousSlice' and 'NextSlice' are set appropriately (one to
+                None, the other to the SliceIndex of the original end contour).
+        '''
+    # pylint: disable=attribute-defined-outside-init
+    def setup_method(self):
+        self.contour_table, self.slice_sequence = make_test_contour_table()
+        self.contour_graph = make_bare_networks_graph_for_testing(self.contour_table, roi=1)
+        self.contour_graph = add_graph_edges(self.contour_graph, self.slice_sequence)
+        self.contour_graph, self.slice_sequence = add_boundary_contours(self.contour_graph, self.slice_sequence)
+
+
+    def test_interpolated_boundary_contour_properties(self):
+        # Find two interpolated boundary contours
+        # (IsInterpolated and IsBoundary True)
+        interpolated = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if data.is_interpolated and data.is_boundary]
+        assert len(interpolated) == 2
+        intp_contour1 = self.contour_graph.nodes[interpolated[0]]['contour']
+        intp_contour2 = self.contour_graph.nodes[interpolated[1]]['contour']
+        # Check flags
+        assert intp_contour1.is_interpolated is True
+        assert intp_contour1.is_boundary is True
+        assert intp_contour2.is_interpolated is True
+        assert intp_contour2.is_boundary is True
+        # Find original contours (should be two for ROI 1)
+        originals = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if not data.is_interpolated and not data.is_boundary]
+        assert len(originals) == 2
+        orig_contour_1 = self.contour_graph.nodes[originals[0]]['contour']
+        orig_contour_2 = self.contour_graph.nodes[originals[1]]['contour']
+        # Check slice index is halfway between original and neighbor
+        slice_indices = [orig_contour_1.slice_index, orig_contour_2.slice_index]
+        expected_slices = [slice_indices[0] - 0.5, slice_indices[1] + 0.5]
+        actual_slices = [intp_contour1.slice_index, intp_contour2.slice_index]
+        assert actual_slices == expected_slices
+        # verify that is_hole matches original
+        assert intp_contour1.is_hole == orig_contour_1.is_hole
+        assert intp_contour2.is_hole == orig_contour_2.is_hole
+        # Area is less than original
+        assert intp_contour1.area < orig_contour_1.area
+        assert intp_contour2.area < orig_contour_2.area
+
+    def test_edge_between_original_and_interpolated(self):
+        '''Test the edge between the original and interpolated contours.
+        verify that:
+            - The ContourMatch contains references to the original contour
+                and the interpolated contour.
+            - The gap is the distance between the original contour and the
+                interpolated contour.
+        '''
+        # Find interpolated boundary contour
+        interpolated = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if data.is_interpolated and data.is_boundary]
+        intp_contour1 = self.contour_graph.nodes[interpolated[0]]['contour']
+        intp_contour2 = self.contour_graph.nodes[interpolated[1]]['contour']
+        # Find original contours (should be two for ROI 1)
+        originals = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if not data.is_interpolated and not data.is_boundary]
+        orig_contour_1 = self.contour_graph.nodes[originals[0]]['contour']
+        orig_contour_2 = self.contour_graph.nodes[originals[1]]['contour']
+        # Find edge between original and interpolated
+        edges1 = [edge[2] for edge in self.contour_graph.edges.data('match')
+                 if interpolated[0] in edge[:2]]
+        match1 = edges1[0]
+        edges2 = [edge[2] for edge in self.contour_graph.edges.data('match')
+                 if interpolated[1] in edge[:2]]
+        match2 = edges2[0]
+        # Check that the original boundary contour is in the match.
+        assert ((match1.contour1 == orig_contour_1) or
+                (match1.contour2 == orig_contour_1))
+        assert ((match2.contour1 == orig_contour_2) or
+                (match2.contour2 == orig_contour_2))
+        # Check that the interpolated boundary contour is in the match.
+        assert ((match1.contour1 == intp_contour1) or
+                (match1.contour2 == intp_contour1))
+        assert ((match2.contour1 == intp_contour2) or
+                (match2.contour2 == intp_contour2))
+        # Verify that the match gap is half the distance between the two slices.
+        expected_gap = abs(orig_contour_1.slice_index -
+                           intp_contour1.slice_index) / 2.0
+        assert ((match1.contour1 == orig_contour_1) or
+                (match1.contour2 == orig_contour_2))
+        assert abs(match1.gap - expected_gap) < 1e-6
+        assert abs(match2.gap - expected_gap) < 1e-6
+
+    def test_slice_sequence_for_interpolated(self):
+        '''Test the changes to the SliceSequence:
+            - The SliceSequence contains the interpolated contour slice index.
+            - The interpolated contour slice index has 'Original' = False.
+            - One of 'PreviousSlice' and 'NextSlice' are set to the
+                SliceIndex of the original end contour.
+            - 'PreviousSlice' and 'NextSlice' of the original end contour do
+                not contain the interpolated slice index.
+        '''
+        # Find interpolated boundary contour
+        interpolated = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if data.is_interpolated and data.is_boundary]
+        intp_contour1 = self.contour_graph.nodes[interpolated[0]]['contour']
+        intp_contour2 = self.contour_graph.nodes[interpolated[1]]['contour']
+        # Find original contours (should be two for ROI 1)
+        originals = [node for node, data in self.contour_graph.nodes.data('contour')
+                        if not data.is_interpolated and not data.is_boundary]
+        orig_contour_1 = self.contour_graph.nodes[originals[0]]['contour']
+        orig_contour_2 = self.contour_graph.nodes[originals[1]]['contour']
+        # Check SliceSequence contains interpolated slice
+        slices = self.slice_sequence.slices
+        assert intp_contour1.slice_index in slices
+        assert intp_contour2.slice_index in slices
+        # Check that interpolated contour slice indexes have 'Original' = False.
+        original_flag = self.slice_sequence.sequence.loc[[
+            intp_contour1.slice_index,
+            intp_contour2.slice_index],
+            'Original']
+        assert all(original_flag == False)
+        # Check that 'PreviousSlice' and 'NextSlice' of the original end
+        # contour do not contain the interpolated slice index.
+        linked_slices1 = list(self.slice_sequence.sequence.loc[
+            orig_contour_1.slice_index,
+            ['PreviousSlice', 'NextSlice']
+            ])
+        linked_slices2 = list(self.slice_sequence.sequence.loc[
+            orig_contour_2.slice_index,
+            ['PreviousSlice', 'NextSlice']
+            ])
+        assert intp_contour1.slice_index not in linked_slices1
+        assert intp_contour2.slice_index not in linked_slices2
+        # Check that one of 'PreviousSlice' and 'NextSlice' are set to the
+        # SliceIndex of the original end contour.
+        linked_slices1 = list(self.slice_sequence.sequence.loc[
+            intp_contour1.slice_index,
+            ['PreviousSlice', 'NextSlice']
+            ])
+        linked_slices2 = list(self.slice_sequence.sequence.loc[
+            intp_contour2.slice_index,
+            ['PreviousSlice', 'NextSlice']
+            ])
+        assert orig_contour_1.slice_index in linked_slices1
+        assert orig_contour_2.slice_index in linked_slices2
 
 class TestBuildContourGraph():
     '''Test the build_contour_graph function.'''
